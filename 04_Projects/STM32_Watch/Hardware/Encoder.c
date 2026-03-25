34b8af83db24632098578f44cac28cf4a26d799a
RitsuYi <smwflrin@gmail.com>
2026-03-25 19:36:48 +0800

    feat：test04

--- 04_Projects/STM32_Watch/Hardware/Encoder.c
+++ 04_Projects/STM32_Watch/Hardware/Encoder.c
@@ -1,6 +1,6 @@
 #include "Encoder.h"
 
-static int16_t Encoder_TransitionCount;
+static volatile int16_t Encoder_TransitionCount;
 static uint8_t Encoder_PreviousState;
 
 static uint8_t Encoder_ReadState(void)
@@ -33,6 +33,40 @@ static void Encoder_UpdateState(void)
     Encoder_PreviousState = currentState;
 }
 
+static void Encoder_EXTIInit(void)
+{
+    EXTI_InitTypeDef EXTI_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource0);
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource10);
+
+    EXTI_ClearITPendingBit(EXTI_Line0 | EXTI_Line10);
+
+    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+    EXTI_Init(&EXTI_InitStructure);
+
+    EXTI_InitStructure.EXTI_Line = EXTI_Line10;
+    EXTI_Init(&EXTI_InitStructure);
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1U;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0U;
+    NVIC_Init(&NVIC_InitStructure);
+
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1U;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
 void Encoder_Init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
@@ -45,24 +79,28 @@ void Encoder_Init(void)
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     Encoder_Reset();
+    Encoder_EXTIInit();
 }
 
 int16_t Encoder_Get(void)
 {
     int16_t stepCount;
 
+    __disable_irq();
     Encoder_UpdateState();
-
     stepCount = Encoder_TransitionCount / 4;
     Encoder_TransitionCount -= (int16_t)(stepCount * 4);
+    __enable_irq();
 
     return stepCount;
 }
 
 void Encoder_Reset(void)
 {
+    __disable_irq();
     Encoder_TransitionCount = 0;
     Encoder_PreviousState = Encoder_ReadState();
+    __enable_irq();
 }
 
 void Encoder_HandleIRQ(void)
--- 04_Projects/STM32_Watch/User/main.c
+++ 04_Projects/STM32_Watch/User/main.c
@@ -7,9 +7,18 @@
 
 typedef enum
 {
+    STATE_HOME = 0,
+    STATE_MENU,
+    STATE_TIMESETTING
+} SystemState;
+
+typedef enum
+{
     HOME_MENU = 0,
     HOME_SETTINGS = 1
 } HomeOption;
+
+#define HOME_OPTION_COUNT 2
 
 static void Watch_FormatDateString(const Time_t *time, char *dateString)
 {
@@ -62,11 +71,65 @@ static void Watch_ShowHomePage(const Time_t *time, HomeOption option)
     OLED_ShowStringMode(4, 12, " SET ", (option == HOME_SETTINGS) ? 1U : 0U);
 }
 
+static void Watch_ShowPlaceholderPage(SystemState state)
+{
+    if (state == STATE_MENU)
+    {
+        OLED_ShowString(1, 4, "ENTER MENU");
+        OLED_ShowString(2, 3, "PHASE 5 NEXT");
+    }
+    else
+    {
+        OLED_ShowString(1, 2, "ENTER SETTING");
+        OLED_ShowString(2, 3, "PHASE 6 NEXT");
+    }
+
+    OLED_ShowString(3, 1, "                ");
+    OLED_ShowString(4, 4, "BTN: BACK");
+}
+
+static HomeOption Watch_MoveHomeOption(HomeOption option, int16_t step)
+{
+    int32_t optionIndex = (int32_t)option + step;
+
+    while (optionIndex < 0)
+    {
+        optionIndex += HOME_OPTION_COUNT;
+    }
+
+    while (optionIndex >= HOME_OPTION_COUNT)
+    {
+        optionIndex -= HOME_OPTION_COUNT;
+    }
+
+    return (HomeOption)optionIndex;
+}
+
+static void Watch_RenderScreen(const Time_t *time, SystemState state, HomeOption option, SystemState *lastRenderedState)
+{
+    if (*lastRenderedState != state)
+    {
+        OLED_Clear();
+        *lastRenderedState = state;
+    }
+
+    if (state == STATE_HOME)
+    {
+        Watch_ShowHomePage(time, option);
+    }
+    else
+    {
+        Watch_ShowPlaceholderPage(state);
+    }
+}
+
 int main(void)
 {
     Time_t currentTime;
     uint32_t lastRtcTick = 0;
     uint8_t lastShownSecond = 0xFFU;
+    SystemState currentState = STATE_HOME;
+    SystemState lastRenderedState = (SystemState)0xFF;
     HomeOption currentOption = HOME_MENU;
 
     OLED_Init();
@@ -78,7 +141,7 @@ int main(void)
 
     lastRtcTick = Timer_GetMillis();
     RTC_GetTime(&currentTime);
-    Watch_ShowHomePage(&currentTime, currentOption);
+    Watch_RenderScreen(&currentTime, currentState, currentOption, &lastRenderedState);
     lastShownSecond = currentTime.second;
 
     while (1)
@@ -95,34 +158,44 @@ int main(void)
             RTC_AdvanceMilliseconds(elapsedMs);
         }
 
-        if (encoderStep != 0)
+        if ((currentState == STATE_HOME) && (encoderStep != 0))
         {
-            if (encoderStep > 0)
+            HomeOption updatedOption = Watch_MoveHomeOption(currentOption, encoderStep);
+
+            if (updatedOption != currentOption)
             {
-                currentOption = HOME_SETTINGS;
+                currentOption = updatedOption;
+                refreshRequired = 1U;
             }
-            else
-            {
-                currentOption = HOME_MENU;
-            }
-            refreshRequired = 1U;
         }
 
         if (buttonEvent != 0U)
         {
+            if (currentState == STATE_HOME)
+            {
+                currentState = (currentOption == HOME_MENU) ? STATE_MENU : STATE_TIMESETTING;
+            }
+            else
+            {
+                currentState = STATE_HOME;
+            }
             refreshRequired = 1U;
         }
 
         RTC_GetTime(&currentTime);
-        if (currentTime.second != lastShownSecond)
+        if ((currentState == STATE_HOME) && (currentTime.second != lastShownSecond))
         {
             lastShownSecond = currentTime.second;
             refreshRequired = 1U;
         }
+        else
+        {
+            lastShownSecond = currentTime.second;
+        }
 
         if (refreshRequired != 0U)
         {
-            Watch_ShowHomePage(&currentTime, currentOption);
+            Watch_RenderScreen(&currentTime, currentState, currentOption, &lastRenderedState);
         }
     }
 }
